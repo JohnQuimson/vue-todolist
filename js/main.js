d8fb9c5cfaa0f824b33a0f279c5f7bdbce48d36c
JohnQuimson <john.quimson777@gmail.com>
2023-11-28 17:29:00 +0100

    feat: aggiunta riga todo

--- js/main.js
+++ js/main.js
@@ -6,7 +6,15 @@ createApp({
   data() {
     return {
       tasks: ['imparare Vue', 'Fare il ToDo list', 'Fare la spesa'],
+      newTask: '',
     };
   },
-  methods() {},
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== '') {
+        this.tasks.push(this.newTask);
+        this.newTask = '';
+      }
+    },
+  },
 }).mount('#app');
